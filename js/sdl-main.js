ab324665920993b81e07398cd912b24b5b1cda5f
Bexon Pak <b1239034902@gmail.com>
2018-12-16 22:25:05 +0800

    Create sdl-main.js

---
+++ js/sdl-main.js
@@ -0,0 +1,31 @@
+$(function() {
+  checkSupport()
+  checkLast()
+})
+
+$('#btn').click(function() {
+  checkSupport()
+  var e = $('#e').val().trim()
+  console.log(e)
+  if(e<=0||e.length == 0) {
+    alert('请输入集数')
+    return false
+  }
+  localStorage.setItem('e', e);
+  self.location.href = 'result.html'
+})
+
+function checkSupport() {
+  if (typeof(Storage) == 'undefined') {
+    alert('请使用版本稍稍新一点的浏览器吧，你这也太旧了，不仅功能太旧而且还不安全哦。')
+    return false
+  }
+}
+
+function checkLast() {
+  var lastE = localStorage.getItem('e')
+  if (lastE != null) {
+    $('#last').text('你上次解析了第' + lastE + '集')
+    $('#last').css('display', 'block')
+  }
+}
